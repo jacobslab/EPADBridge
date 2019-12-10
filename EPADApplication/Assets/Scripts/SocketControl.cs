43c71f5f3b0c3f46d9f0bfc0157bb8610c01e1f6
Ansh Patel <lightnarcissus@gmail.com>
2019-12-10 15:03:37 -0500

    added server side changes

--- EPADApplication/Assets/Scripts/SocketControl.cs
+++ EPADApplication/Assets/Scripts/SocketControl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Net;
 using System.Net.Sockets;
+using System;
 using System.Text;
 using System.Threading;
 using System.Collections.Generic;
@@ -16,16 +17,147 @@ public class ServerThread : ThreadedJob
 
     protected override void ThreadFunction()
     {
-
+        //StartListening();
         ServerSocket();
-        ListeningServer();
+        //ListeningServer();
+        TCPListener();
+    }
+
+    void TCPListener()
+    {
+        TcpListener server = null;
+        try
+        {
+            // Set the TcpListener on port 13000.
+            Int32 port = 9999;
+            IPAddress localAddr = IPAddress.Parse("192.168.0.102");
+
+            // TcpListener server = new TcpListener(port);
+            server = new TcpListener(localAddr, port);
+
+            // Start listening for client requests.
+            server.Start();
+
+            // Buffer for reading data
+            Byte[] bytes = new Byte[256];
+            String data = null;
+
+            // Enter the listening loop.
+            while (true)
+            {
+                Debug.Log("Waiting for a connection... ");
+
+                // Perform a blocking call to accept requests.
+                // You could also user server.AcceptSocket() here.
+                TcpClient client = server.AcceptTcpClient();
+                Debug.Log("Connected!");
+
+                data = null;
+
+                // Get a stream object for reading and writing
+                NetworkStream stream = client.GetStream();
+
+                int i;
+
+                // Loop to receive all the data sent by the client.
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    // Translate data bytes to a ASCII string.
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Debug.Log("Received: " + data);
+
+                    // Process the data sent by the client.
+                    data = data.ToUpper();
+
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                    // Send back a response.
+                    stream.Write(msg, 0, msg.Length);
+                    Debug.Log("Sent: " + data);
+                }
+
+                // Shutdown and end connection
+                client.Close();
+            }
+        }
+        catch (SocketException e)
+        {
+           Debug.Log("SocketException: " + e);
+        }
+        finally
+        {
+            // Stop listening for new clients.
+            server.Stop();
+        }
     }
 
     protected override void OnFinished()
     {
     }
 
-    void ListeningServer()
+        // Incoming data from the client.  
+        public static string data = null;
+
+        public static void StartListening()
+        {
+            // Data buffer for incoming data.  
+            byte[] bytes = new byte[1024];
+
+            // Establish the local endpoint for the socket.  
+            // Dns.GetHostName returns the name of the   
+            // host running the application.  
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[1];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress,9999);
+
+            // Create a TCP/IP socket.  
+            Socket listener = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the local endpoint and   
+            // listen for incoming connections.  
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                // Start listening for connections.  
+                while (true)
+                {
+                    Debug.Log("Waiting for a connection...");
+                    // Program is suspended while waiting for an incoming connection.  
+                    Socket handler = listener.Accept();
+                    data = null;
+
+                    // An incoming connection needs to be processed.  
+                    while (true)
+                    {
+                        int bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                   
+                    }
+
+                    // Show the data on the console.  
+                    Debug.Log("Text received : " + data);
+
+                    // Echo the data back to the client.  
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.ToString());
+            }
+
+
+        }
+
+        void ListeningServer()
     {
         Debug.Log("starting listening server");
         Socket listenSocket = new Socket(AddressFamily.InterNetwork,
@@ -44,13 +176,16 @@ public class ServerThread : ThreadedJob
         listenSocket.Listen(backlog);
 
         Debug.Log("about to accept");
-        listenSocket.Accept();
+        Socket s  = listenSocket.Accept();
+        Debug.Log("accepted");
         SocketAsyncEventArgs e = new SocketAsyncEventArgs();
 
         byte[] bytes = new byte[256];
-        int byteCount  = listenSocket.Receive(bytes,0,listenSocket.Available, SocketFlags.None);
+        Debug.Log("about to receive");
+        int byteCount  = s.Receive(bytes,0,s.Available, SocketFlags.None);
 
-        if(byteCount > 0)
+        Debug.Log("byte count " + byteCount.ToString());
+        if (byteCount > 0)
         {
             Debug.Log(Encoding.UTF8.GetString(bytes));
         }
